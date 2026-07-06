d003029feea671817156b8d34b7894febf12d97b
nitrogen-12 <119827347+swisd@users.noreply.github.com>
2026-07-06 02:36:12 +0000

    fix2

--- csvsexec.sh
+++ csvsexec.sh
@@ -1,5 +1,7 @@
 # 1. Install the toolchain bundler
 curl https://github.io -sSf | sh
 
+export PATH="$HOME/.cargo/bin:$PATH"
+
 # 2. Compile and output directly into your web subfolder
 wasm-pack build --target web --out-dir web/pkg
