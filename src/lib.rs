8fc86e4c175c336bfe98edee80506d5298818e68
nitrogen-12 <119827347+swisd@users.noreply.github.com>
2026-07-06 21:42:43 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,6 @@ use wasm_bindgen::prelude::*;
 pub fn compile_from_web(source_code: &str, target_arch: &str) -> String {
     // Bridges the string from JavaScript into your internal no_std compiler pipeline
     match crate::compiler::compile(source_code, target_arch) {
-        Ok(assembly) => assembly,
-        Err(e) => alloc::format!("Compilation Error: {:?}", e),
+        assembly => assembly
     }
 }
