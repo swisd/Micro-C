190f839d975cfa6c9dbd9ba0cb06917d0f3d2d8b
swisd <swisd@users.noreply.github.com>
2026-07-09 16:47:38 +0000

    deploy: d489864169c848e7eb565ca461cad4a674dc88f2

--- pkg/micro_c_bg.wasm.d.ts
+++ pkg/micro_c_bg.wasm.d.ts
@@ -3,8 +3,8 @@
 export const memory: WebAssembly.Memory;
 export const compile_from_web: (a: number, b: number, c: number, d: number) => [number, number];
 export const get_errors: () => [number, number];
-export const clear_errors: () => void;
 export const init_errors: () => void;
+export const clear_errors: () => void;
 export const __wbindgen_externrefs: WebAssembly.Table;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
